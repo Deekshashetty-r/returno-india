7a26306d846dde7fc0dd2959133e62afaa6acbcc
Deekshashetty-r <deekshashetty759@gmail.com>
2026-05-28 11:14:17 +0530

    fix css import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
-import '../app/globals.css'
+// @ts-ignore
+import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
